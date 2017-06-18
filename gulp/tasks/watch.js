a93ad60fa1cc7cd99741868b9ce5b50def6b6220
Ulysse <ulyssecoutu@gmail.com>
2017-06-18 11:03:03 -0400

    automatic sprite

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,25 +2,26 @@ var gulp = require('gulp'),
 watch = require('gulp-watch'),
 browserSync = require('browser-sync').create();
 
-gulp.task('watch',function(){
+gulp.task('watch', function() {
 
   browserSync.init({
-    notify:false,
-    server:{
-      baseDir:"app"
+    notify: false,
+    server: {
+      baseDir: "app"
     }
   });
 
-  watch('./app/index.html', function(){
+  watch('./app/index.html', function() {
     browserSync.reload();
   });
 
-  watch('./app/assets/styles/**/*.css', function(){
+  watch('./app/assets/styles/**/*.css', function() {
     gulp.start('cssInject');
   });
+
 });
 
-gulp.task('cssInject', ['styles'], function(){
+gulp.task('cssInject', ['styles'], function() {
   return gulp.src('./app/temp/styles/styles.css')
-  .pipe(browserSync.stream());
-})
+    .pipe(browserSync.stream());
+});
